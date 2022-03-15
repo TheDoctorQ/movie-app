5cae61b26ad429ae36b439004028bc9bdf960a5d
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-14 22:15:06 -0600

    adds controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+actor.save
