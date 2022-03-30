8641c491d90e23d362d8a01fe592a1524cbccb53
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-29 22:54:21 -0600

    adds validations to actor model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,6 @@
 class Actor < ApplicationRecord
+  validates :first_name, length: { minimum: 2 }
+  validates :last_name, length: { minimum: 2 }
+  validates :known_for, presence: true
+  validates :age, comparison: { greater_than: 13 }
 end
