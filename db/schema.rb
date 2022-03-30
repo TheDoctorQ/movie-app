863d8a053996e542e0e67c95f032d2366cc0df01
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-29 22:23:53 -0600

    adds director and language boolean to movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_235756) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_30_040627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_235756) do
     t.text "plot"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
+    t.boolean "english", default: true
   end
 
 end
