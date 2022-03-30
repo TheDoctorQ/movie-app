2ee323fd84f15b9ab4561192ba67a17652013a8f
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-29 22:55:29 -0600

    adds validations to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
-
-
-
-
+  validates :title, length: { minimum: 2}
+  validates :director, length: { minimum: 2}
+  validates :year, length: { minimum: 4}
+  validates :plot, length: { minimum: 10}
 end
